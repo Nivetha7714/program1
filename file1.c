bcec620e4f5e066f536566b71e740191f46ae8c5
Nivetha7714 <nivethakrishnan1321@gmail.com>
2016-06-07 10:21:16 +0530

    Create file1.c

---
+++ file1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int a;
+	scanf("%d",&a);
+	if(a%2==0)
+	{
+		printf("the no is even");
+	}
+	else
+	{
+		printf("the no is odd ");
+	}
+	return 0;
+}
